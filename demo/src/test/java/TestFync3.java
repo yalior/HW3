3899ee5bda1e2a4d139f0ddd9a983092f59908aa
yalior <yakovlevlior@gmail.com>
2026-01-06 16:01:56 +0200

    commit

--- demo/src/test/java/TestFync3.java
+++ demo/src/test/java/TestFync3.java
@@ -7,8 +7,4 @@ public class TestFync3 {
     public void testFunc3First() {
         demo.src.main.java.com.example.Main.func3(5);
     }
-    @Test(expected = ArithmeticException.class)
-    public void testFunc3Second() {
-        demo.src.main.java.com.example.Main.func3(0);
-    }
 }
